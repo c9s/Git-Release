5ae3d00519aa84d887926ca21244b872ba436ee3
c9s <cornelius.howl@gmail.com>
2011-10-07 13:50:31 +0800

    use config method from Git.

--- lib/Git/Release.pm
+++ lib/Git/Release.pm
@@ -1,17 +1,18 @@
 package Git::Release;
 use warnings;
 use strict;
+use Git;
 use Cwd;
 
 sub new { 
     my $self = {};
     bless $self , shift;
-
     my $working_dir = getcwd();
     my $repo = Git->repository ( WorkingSubdir => $working_dir );
     $self->{repo} = $repo;
     return $self;
 }
+
 
 
 1;
