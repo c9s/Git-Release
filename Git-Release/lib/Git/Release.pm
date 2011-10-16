1c83eb592acce7e5e2f62bc2b529cead4f058074
c9s <cornelius.howl@gmail.com>
2011-10-16 14:55:19 +0800

    lib files

---
+++ Git-Release/lib/Git/Release.pm
@@ -0,0 +1,32 @@
+package Git::Release;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Git::Release -
+
+=head1 SYNOPSIS
+
+  use Git::Release;
+
+=head1 DESCRIPTION
+
+Git::Release is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
