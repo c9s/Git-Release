3d9c4f55f8b1c13b849eb97ccf523e3fd7346b1f
c9s <cornelius.howl@gmail.com>
2011-10-16 21:54:17 +0800

    Add a basic test

--- Git-Release/lib/Git/Release.pm
+++ Git-Release/lib/Git/Release.pm
@@ -2,6 +2,55 @@ package Git::Release;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+use feature qw(say switch);
+use Mo;
+use Cwd;
+use Git;
+use Getopt::Long;
+use List::MoreUtils qw(uniq);
+use DateTime;
+
+has repo => (  );
+
+sub new {
+    my ($class,%args) = @_;
+    my $self = bless {} ,$class;
+    my $repo = Git->repository ( WorkingSubdir => $args{working_dir} || getcwd() );
+    $self->repo( $repo );
+    return $self;
+}
+
+# list all remote, all local branches
+sub get_all_branches {
+    my $repo = shift;
+
+    # remove remtoes names, strip star char.
+    return uniq 
+            map { chomp; $_; } 
+            map { s/^\*?\s*//; $_; } 
+                $repo->command( 'branch' , '-a' );
+}
+
+sub get_local_branches {
+    my $repo = shift;
+    return map { chomp; $_; } 
+           map { s/^\*?\s*//; $_; } 
+           $repo->command( 'branch' , '-l' );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 1;
 __END__
--- Git-Release/t/00_compile.t
+++ Git-Release/t/00_compile.t
@@ -2,3 +2,9 @@ use strict;
 use Test::More tests => 1;
 
 BEGIN { use_ok 'Git::Release' }
+
+my $re = Git::Release->new( );
+
+ok( $re );
+ok( $re->repo );
+is( 'Git', ref( $re->repo ) );
