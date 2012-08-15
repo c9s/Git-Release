4c47f2d08b0ebd540dfeed55fb4312271b534f15
c9s <cornelius.howl@gmail.com>
2012-08-16 00:53:31 +0800

    Add RemoteManager

---
+++ lib/Git/Release/RemoteManager.pm
@@ -0,0 +1,20 @@
+package Git::Release::RemoteManager;
+use Moose;
+
+has manager => ( is => 'rw' );
+
+sub repo { return $_[0]->manager->repo; }
+
+sub add { 
+	my ($self,$name,$uri) = @_;
+	$self->manager->repo->command('remote','add',$name,$uri);
+}
+
+sub all {
+	my $self = shift;
+    # provide a list context to get remote names
+    my @remotes = $self->manager->repo->command('remote');
+    return @remotes;
+}
+
+1;
